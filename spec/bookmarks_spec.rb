a10a25200387e857365277afecbd54747791cb87
Laura Petre <laura.d.petre@gmail.com>
2018-06-04 16:15:09 +0100

    Added 2nd user story, installed psql

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -2,8 +2,10 @@ require "bookmarks"
 
 describe Bookmarks do
 
-  it "should have some bookmarks" do 
+  it "should have some bookmarks" do
     expect(Bookmarks.all).to include("www.google.com")
+    expect(Bookmarks.all).to include("www.reddit.com")
+    expect(Bookmarks.all).to include("www.twitter.com")
   end
 
 end
